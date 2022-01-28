ce53f8d2464412a30f7e73f4f9ac7e2315938e73
kwdaaa <kwd.aaa.a@icloud.com>
2022-01-29 08:49:16 +0900

    タイトル： 噛み砕きが完了しました。
    詳細：
    コメントアウトで各コードが何を行っているのか記載しています。

--- A4.py
+++ A4.py
@@ -6,3 +6,8 @@ print(members[0])
 
 # 変数「members」のインデント番号「1」の中身を出力。
 print(members[1])
+
+print(members[:2])
+
+# 内包表記
+print('\n'.join(member for member in members if member == 'Bob' or member == 'Ken'))
